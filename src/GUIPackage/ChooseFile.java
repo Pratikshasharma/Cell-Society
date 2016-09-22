d905b946e9b186beaaf834f0b3802d102d267e6e
Pratiksha Sharma <ps179@duke.edu>
2016-09-22 15:21:39 -0400

    Added Class to choose File

---
+++ src/GUIPackage/ChooseFile.java
@@ -0,0 +1,33 @@
+package GUIPackage;
+import java.io.File;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class ChooseFile{
+	private FileChooser fileChooser = new FileChooser();
+	private Stage myStage;
+
+	public File chooseFile(){
+		FileChooser.ExtensionFilter extentionFilter = new FileChooser.ExtensionFilter("XML files (*.XML)", "*.XML");		
+		
+		String userDirectoryString = System.getProperty("user.dir") + File.separator;
+		File userDirectory = new File(userDirectoryString) ;
+		fileChooser.setInitialDirectory(userDirectory);
+		fileChooser.getExtensionFilters().add(extentionFilter);
+		File chosenFile = fileChooser.showOpenDialog(myStage);
+		
+		if(chosenFile == null) {
+			GUIController.exitGame();
+		}
+		return chosenFile;		
+	}
+
+	public FileChooser getFileChooser() {
+		return this.fileChooser;
+		
+	}	
+
+
+}
+
+
