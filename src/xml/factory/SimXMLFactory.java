098a44a8411659045a27fb1d11b954a25860c015
Ryan Anders <dra17@duke.edu>
2016-10-02 14:56:38 -0400

    added checks for genParams being null or being invalid inputs

--- src/xml/factory/SimXMLFactory.java
+++ src/xml/factory/SimXMLFactory.java
@@ -6,6 +6,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javafx.scene.paint.Color;
+
 import java.util.Objects;
 
 
@@ -78,17 +80,45 @@ public abstract class SimXMLFactory extends XMLFactory {
     		Node nNode = nList.item(temp);
     		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
     			Element eElement = (Element) nNode;
+    			valueCheck(eElement,temp);
     			states[temp] = new GenState(getTextValue(eElement, "statename"), getTextValue(eElement, "statecolor"),
-    					getTextValue(eElement, "percentage"), eElement.getAttribute("id"));
+    					getTextValue(eElement, "percentage"), eElement.getAttribute("id"));    			
+    			
     			checkPercentages += Double.parseDouble(getTextValue(eElement, "percentage"));
     		}
     	}
     	
     	if (checkPercentages != 1) {
-    		throw new XMLFactoryException("State Percentages add up to %.2f, not 1", checkPercentages);
+    		throw new XMLFactoryException("State Percentages add up to %.2f, must add up to 1", checkPercentages);
     	}
     	
     	return states;
     }
-
+    
+    @SuppressWarnings("unused")
+	private void valueCheck(Element e, int n) throws XMLFactoryException{
+    	String name = getTextValue(e, "statename");
+    	String color = getTextValue(e, "statecolor");
+    	String percentage = getTextValue(e, "percentage");
+    	String id = e.getAttribute("id");
+    	
+    	if(name.equals("")) {throw new XMLFactoryException("State %d: no name found", n+1);}
+    	if(color.equals("")) {throw new XMLFactoryException("State %d: no color found", n+1);}
+    	if(percentage.equals("")) {throw new XMLFactoryException("State %d: no percentage found", n+1);}
+    	if(id.equals("")) {throw new XMLFactoryException("State %d: no state ID found", n+1);}
+    	
+    	try {
+			Color c = Color.valueOf(color);
+    		double d = Double.parseDouble(percentage);
+    		int i = Integer.parseInt(id);
+    	} catch (IllegalArgumentException exc) {
+    		if (exc.getMessage().equals("For input string: \""+percentage+"\"")) {
+    			throw new XMLFactoryException("State %d: Percentage '%s' is invalid, must be a value between 0 and 1", n+1, percentage);
+    		} else if (exc.getMessage().equals("For input string: \""+id+"\"")){
+    			throw new XMLFactoryException("State %d: ID '%s' is invalid, must be an integer", n+1, id);	
+    		} else if (exc.getMessage().equals("Invalid color specification")) {
+    			throw new XMLFactoryException("State %d: Color '%s' is not recognized", n+1, color);
+    		}
+    	} 
+    }
 }
